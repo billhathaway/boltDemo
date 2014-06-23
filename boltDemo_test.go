ec991234825d9b61bcd3ec82a4d131bf949bcb05
Bill Hathaway <bill.hathaway@gmail.com>
2014-06-22 18:14:12 -0700

    add t.Log statements to give feedback about progress when test -v is used

--- boltDemo_test.go
+++ boltDemo_test.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	//"encoding/json"
+	"encoding/gob"
+	"encoding/json"
 	"io/ioutil"
 	"net/http"
 	"net/url"
 	"os"
+	"strconv"
 	"strings"
 	"testing"
 	"time"
@@ -26,6 +29,8 @@ func validateResponse(t *testing.T, response *http.Response, err error, lookFor 
 	if err != nil {
 		t.Fatalf("%s - %s\n", message, err.Error())
 	}
+	defer response.Body.Close()
+
 	if response.StatusCode != 200 {
 		t.Fatalf("%s - received invalid status code %d\n", message, response.StatusCode)
 	}
@@ -36,7 +41,8 @@ func validateResponse(t *testing.T, response *http.Response, err error, lookFor 
 	if !strings.Contains(string(content), lookFor) {
 		t.Fatalf("%s - looking for %q but did not find it response=%q\n", message, lookFor, content)
 	}
-	response.Body.Close()
+	t.Logf("completed %s", message)
+
 }
 
 func TestQueueOperations(t *testing.T) {
@@ -45,8 +51,7 @@ func TestQueueOperations(t *testing.T) {
 	httpResponse, err := client.PostForm(baseURL+"queue/q1", nil)
 	validateResponse(t, httpResponse, err, "created queue", "creating queue")
 
-	messageAttr := url.Values{}
-	messageAttr.Add("message", "content")
+	messageAttr := url.Values{"message": []string{"content"}}
 	httpResponse, err = client.PostForm(baseURL+"queue/q1/messages", messageAttr)
 	validateResponse(t, httpResponse, err, "msgId", "sending message")
 
@@ -59,4 +64,28 @@ func TestQueueOperations(t *testing.T) {
 	httpResponse, err = client.Get(baseURL + "queue/q1")
 	validateResponse(t, httpResponse, err, `"MessageCount":0`, "get queue info - 0 messages")
 
+}
+
+func BenchmarkGobMessage(b *testing.B) {
+	m := Message{Id: strconv.FormatInt(time.Now().UnixNano(), 10), Data: "small value"}
+	encoder := gob.NewEncoder(ioutil.Discard)
+	var err error
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		if err = encoder.Encode(m); err != nil {
+			b.Error("problem encoding", err)
+		}
+	}
+}
+
+func BenchmarkJSONMessage(b *testing.B) {
+	m := Message{Id: strconv.FormatInt(time.Now().UnixNano(), 10), Data: "small value"}
+	encoder := json.NewEncoder(ioutil.Discard)
+	var err error
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		if err = encoder.Encode(m); err != nil {
+			b.Error("problem encoding", err)
+		}
+	}
 }
